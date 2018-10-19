02689ae7377864ad70c73930bec220d91391c91d
quangduyvo <quangvoduy484@gmail.com>
2018-10-19 09:59:06 +0700

    init_project

---
+++ QLTourDucLich/QLTourDucLich/Controllers/TourController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLTourDucLich.Models;
+
+namespace QLTourDucLich.Controllers
+{
+    public class TourController : Controller
+    {
+        //
+        // GET: /Tour/
+        QlTourDuLichEntities ql = new QlTourDuLichEntities();
+
+        public ActionResult HienThiTour()
+        {
+
+
+
+            return View(ql.TOURs.Take(8).ToList());
+        }
+
+    }
+}
---
+++ QLTourDucLich/QLTourDucLich/Models/DSTour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLTourDucLich.Models
+{
+    public class DSTour
+    {
+       public QlTourDuLichEntities ql;
+        public string TenTour { get; set; }
+        public string TenDiaDiem { get; set; }
+        public string HinhAnh { get; set; }
+        public  DSTour()
+        {
+            ql = new QlTourDuLichEntities();
+            var tour = (from t in ql.TOURs
+                        join ht in ql.HANHTRINHs on t.MaHanhTrinh equals ht.MaHanhTrinh
+                        join dd in ql.DIADIEMs on ht.NoiDen equals dd.MaDiaDiem
+                        //   where t.LOAITOUR=="1"
+
+                        select new
+                        {
+                            TenTour = t.TenTour,
+                            HinhAnh = t.AnhDaiDien,
+                            TenDiaDiem = dd.TenDiaDiem
+                        }).Take(1);
+            
+
+
+        }
+    }
+}
